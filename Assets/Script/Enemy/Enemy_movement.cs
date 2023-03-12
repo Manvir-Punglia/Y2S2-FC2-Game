0619b9e86f02d05c9d518ba18465ab29f1f468a0
ChiaLoChen <chialo.chen@ontariotechu.net>
2023-03-12 18:32:35 -0400

    dungeon 2 and 3 setup

--- Assets/Script/Enemy/Enemy_movement.cs
+++ Assets/Script/Enemy/Enemy_movement.cs
@@ -23,7 +23,6 @@ public class Enemy_movement : MonoBehaviour
 
     //public ParticleSystem fireballParticles;
 
-    public bool canSeeTarget;
     public bool hit;
 
     public float runDistance;
@@ -33,14 +32,10 @@ public class Enemy_movement : MonoBehaviour
     float time = 0;
 
     public float health;
+    public float speed;
 
     public int bounty;
     bool bountyObtain;
-
-    public float wanderRadius;
-    float timer = 0;
-    public float wanderingTimer;
-    public float speed;
 
     public bool canShoot;
     public float bulletSpeed;
@@ -56,7 +51,6 @@ public class Enemy_movement : MonoBehaviour
     }
     private void Start()
     {
-        hit = false;
         player = GameObject.FindWithTag("Player");
         banner = GameObject.FindGameObjectWithTag("Player").GetComponent<bannerManager>();
         animator = GetComponent<Animator>();
@@ -70,81 +64,70 @@ public class Enemy_movement : MonoBehaviour
     private void Update()
     {
         agent.speed = speed;
+        transform.LookAt(player.transform.position);
+        agent.SetDestination(player.transform.position);
         Vector3 runTo = transform.position + ((transform.position - player.transform.position));
         float distance = Vector3.Distance(transform.position, player.transform.position);
-        animator.SetFloat("Movement", 0);
+        animator.SetFloat("Movement", 1);
         Die();
-        if (canSeeTarget)
+        switch (type)
         {
-            transform.LookAt(player.transform.position);
-            animator.SetFloat("Movement", 1);
-            switch (type)
-            {
-                case enemy.MELEE:
+            case enemy.MELEE:
+                {
+                    agent.destination = player.transform.position;
+                    if (!hit)
                     {
-                        if (!hit)
+                        if (time >= hitTime)
                         {
-                            agent.destination = player.transform.position;
-                            animator.SetBool("ATK_Melee", true);
+                            hit = true;
+                            time = 0;
                         }
-                        else
+                        if (time < hitTime)
                         {
-                            if (time < hitTime)
+                            time += Time.deltaTime;
+                            if (distance < runDistance)
                             {
-                                animator.SetBool("ATK_Melee", false);
-                                if (distance < runDistance)
-                                {
-                                    agent.SetDestination(runTo);
-                                }
+                                agent.SetDestination(runTo);
+                            }
+                        }
+                    }
 
-                                else
-                                {
-                                    agent.destination = player.transform.position;
-                                }
-                                time += Time.deltaTime;
-                            }
-                            else
-                            {
-                                hit = false;
-                                time = 0;
-                            }
-                        }
-                    }
-                    break;
-                case enemy.RANGE:
+                }
+                break;
+            case enemy.RANGE:
+                {
+                    agent.stoppingDistance = shootingDistance;
+                    canShootTimer += Time.deltaTime;
+                    if (canShoot)
                     {
-                        if (distance < shootingDistance)
-                        {
-                            agent.SetDestination(runTo);
-                        }
-                        else
-                        {
-                            agent.destination = player.transform.position;
-                        }
-                        Shooting();
+                        var BulletClone = Instantiate(bullet, shootingPos.transform.position, Quaternion.identity);
+                        //var FireballClone = Instantiate(fireballParticles, BulletClone.transform.position, Quaternion.identity);
+                        //FireballClone.transform.parent = BulletClone.transform;
+
+                        BulletClone.GetComponent<Rigidbody>().AddForce((player.transform.position - shootingPos.transform.position).normalized * bulletSpeed);
+                        //fireballParticles.Play();
+
+                        canShoot = false;
+                        animator.SetBool("ATK_Range", true);
+
                     }
-                    break;
-                case enemy.MONEY:
+                    if (canShootTimer >= shootTimer && (transform.position - player.transform.position).magnitude >= shootingDistance)
                     {
-                        if (distance < runDistance)
-                        {
-                            agent.SetDestination(runTo);
-                        }
+                        canShoot = true;
+                        canShootTimer = 0;
                     }
-                    break;
-            }
-        }
-        else
-        {
-            Wandering();
+                    if (animator.GetCurrentAnimatorStateInfo(0).IsName("ATK_Range"))
+                    {
+                        agent.isStopped = true;
+                    }
+                }
+                break;
         }
     }
     public void Die()
     {
         if (health <= 0)
         {
-            canSeeTarget = false;
-            hit = false;
             if (!bountyObtain)
             {
                 banner.increaseKillCount();
@@ -160,65 +143,19 @@ public class Enemy_movement : MonoBehaviour
     {
         health -= dmg;
     }
-
-    void Shooting()
-    {
-        canShootTimer += Time.deltaTime;
-        if (canShoot)
-        {
-            var BulletClone = Instantiate(bullet, shootingPos.transform.position, Quaternion.identity);
-            //var FireballClone = Instantiate(fireballParticles, BulletClone.transform.position, Quaternion.identity);
-            //FireballClone.transform.parent = BulletClone.transform;
-
-            BulletClone.GetComponent<Rigidbody>().AddForce((player.transform.position - shootingPos.transform.position).normalized * bulletSpeed);
-            //fireballParticles.Play();
-
-            canShoot = false;
-            animator.SetBool("ATK_Range", true);
-
-        }
-        if (canShootTimer >= shootTimer && (transform.position - player.transform.position).magnitude >= shootingDistance)
-        {
-            canShoot = true;
-            canShootTimer = 0;
-        }
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("ATK_Range"))
-        {
-            agent.isStopped = true;
-        }
-    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == ("Bullet"))
         {
             StartCoroutine(hitAnim.HitAnim());
-            canSeeTarget = true;
             health -= collision.gameObject.GetComponent<bullet>().getDamage();
 
         }
-        if (collision.collider.CompareTag("Player"))
+        if (collision.gameObject.tag == ("Player"))
         {
-            hit = true;
+            hit = false;
+            animator.SetBool("ATK_Melee", true);
             collision.gameObject.GetComponent<PlayerManager>().TakeDamage();
-        }
-    }
-    void Wandering()
-    {
-        timer += Time.deltaTime;
-
-        if (timer >= wanderingTimer)
-        {
-            Vector3 pos;
-            float ang = Random.value * 360;
-            pos.x = transform.position.x + (wanderRadius * Mathf.Sin(ang * Mathf.Deg2Rad));
-            pos.y = transform.position.y;
-            pos.z = transform.position.z + (wanderRadius * Mathf.Cos(ang * Mathf.Deg2Rad));
-            agent.SetDestination(pos);
-            timer = 0;
-        }
-        if ((transform.position - agent.destination).magnitude > 0)
-        {
-            animator.SetFloat("Movement", 1);
         }
     }
     //public void ShootFireBall()
@@ -232,15 +169,6 @@ public class Enemy_movement : MonoBehaviour
     //    fireballParticles.Play();
 
     //}
-
-    public bool GetCanSee()
-    {
-        return canSeeTarget;
-    }
-    public void SetCanSee(bool canSee)
-    {
-        canSeeTarget = canSee;
-    }
     public void SetHit(bool isHit)
     {
         hit = isHit;
